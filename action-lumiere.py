e822a1105f78d425d6bcbc52a38c03091ae75d9f
Fabrice <thomasmartins83@gmail.com>
2019-06-20 14:46:20 +0200

    test_leds1

--- action-lumiere.py
+++ action-lumiere.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python2
 from hermes_python.hermes import Hermes
+import apa102
 
 MQTT_IP_ADDR = "localhost"
 MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
-def intent_received(hermes, intent_message):
-    print()
-    print(intent_message.intent.intent_name)
-    print()
+class Pixels:
+    PIXELS_N = 3
+
+    def __init__(self):
+        self.colors = [0] * 3 * self.PIXELS_N
+        self.dev = apa102.APA102(num_led=self.PIXELS_N)
 
 
-with Hermes(MQTT_ADDR) as h:
-    h.subscribe_intents(intent_received).start()
+
+    def intent_received(self, hermes, intent_message):
+        print()
+        print(intent_message.intent.intent_name)
+        colors = self.colors
+        self.dev.set_pixel(3, int(colors[3 * 3]), int(colors[3 * 3 + 1]), int(colors[3 * 3 + 2]))
+        print()
+
+
+    with Hermes(MQTT_ADDR) as h:
+        h.subscribe_intents(intent_received).start()
