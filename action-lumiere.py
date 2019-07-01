2b322f3ffd89ce2125e3f0276b5c59979086e395
Fabrice <thomasmartins83@gmail.com>
2019-07-01 12:10:41 +0200

    ajout end_session

--- action-lumiere.py
+++ action-lumiere.py
@@ -3,9 +3,7 @@
 """
 LED light pattern like Google Home
 """
-import apa102
-import time
-import threading
+import pixel
 import requests
 
 try:
@@ -20,127 +18,15 @@ MQTT_PORT = 1883
 MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
-class Pixels:
-    PIXELS_N = 3
+pixels = pixel.Pixels()
 
-    def __init__(self):
-        self.basis = [0] * 3 * self.PIXELS_N
-        self.basis[0] = 2
-        self.basis[3] = 1
-        self.basis[4] = 1
-        self.basis[7] = 2
-
-        self.colors = [0] * 3 * self.PIXELS_N
-        self.dev = apa102.APA102(num_led=self.PIXELS_N)
-
-        self.next = threading.Event()
-        self.queue = Queue.Queue()
-        self.thread = threading.Thread(target=self._run)
-        self.thread.daemon = True
-        self.thread.start()
-
-    def wakeup(self, direction=0):
-        def f():
-            self._wakeup(direction)
-
-        self.next.set()
-        self.queue.put(f)
-
-    def listen(self):
-        self.next.set()
-        self.queue.put(self._listen)
-
-    def think(self):
-        self.next.set()
-        self.queue.put(self._think)
-
-    def speak(self):
-        self.next.set()
-        self.queue.put(self._speak)
-
-    def off(self):
-        self.next.set()
-        self.queue.put(self._off)
-
-    def _run(self):
-        while True:
-            func = self.queue.get()
-            func()
-
-    def _wakeup(self, direction=0):
-        for i in range(1, 25):
-            colors = [i * v for v in self.basis]
-            self.write(colors)
-            time.sleep(0.01)
-
-        self.colors = colors
-
-    def _listen(self):
-        for i in range(1, 25):
-            colors = [i * v for v in self.basis]
-            self.write(colors)
-            time.sleep(0.01)
-
-        self.colors = colors
-
-    def _think(self):
-        colors = self.colors
-
-        self.next.clear()
-        while not self.next.is_set():
-            colors = colors[3:] + colors[:3]
-            self.write(colors)
-            time.sleep(0.2)
-
-        t = 0.1
-        for i in range(0, 5):
-            colors = colors[3:] + colors[:3]
-            self.write([(v * (4 - i) / 4) for v in colors])
-            time.sleep(t)
-            t /= 2
-
-        # time.sleep(0.5)
-
-        self.colors = colors
-
-    def _speak(self):
-        colors = self.colors
-        gradient = -1
-        position = 24
-
-        self.next.clear()
-        while not self.next.is_set():
-            position += gradient
-            self.write([(v * position / 24) for v in colors])
-
-            if position == 24 or position == 4:
-                gradient = -gradient
-                time.sleep(0.2)
-            else:
-                time.sleep(0.01)
-
-        while position > 0:
-            position -= 1
-            self.write([(v * position / 24) for v in colors])
-            time.sleep(0.01)
-
-        # self._off()
-
-    def _off(self):
-        self.write([0] * 3 * self.PIXELS_N)
-
-    def write(self, colors):
-        for i in range(self.PIXELS_N):
-            self.dev.set_pixel(i, int(colors[3 * i]), int(colors[3 * i + 1]), int(colors[3 * i + 2]))
-
-        self.dev.show()
-
-
-pixels = Pixels()
-
-def init_wakeword(hermes, intent_message):
+def init_wakeword(hermes):
     pixels.listen()
     print('WAKEWORD DETECTED')
+
+def end_session(hermes):
+    pixels.off();
+    print('END OF THE SESSION')
 
 def intent_received(hermes, intent_message):
     print()
@@ -197,4 +83,5 @@ def intent_received(hermes, intent_message):
 
 with Hermes(MQTT_ADDR) as h:
     h.subscribe_intents(intent_received)\
-        .subscribe_session_started(init_wakeword).start()
+        .subscribe_session_started(init_wakeword)\
+        .subscribe_session_ended(end_session).start()
