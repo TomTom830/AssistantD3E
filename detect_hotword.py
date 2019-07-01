6d00ac7a03fb61723ba5afe5139daf13f0d1ea9e
Fabrice <thomasmartins83@gmail.com>
2019-07-01 11:23:21 +0200

    ajout leds test hotword

--- detect_hotword.py
+++ detect_hotword.py
@@ -1,14 +1,20 @@
 import paho.mqtt.client as mqtt
+import pixel
+
 
 HOST = 'localhost'
 PORT = 1883
+pix = pixel.Pixels()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected to {0} with result code {1}".format(HOST, rc))
+    # Subscribe to the hotword detected topic
+    client.subscribe("hermes/hotword/default/detected")
 
 def on_message(client, userdata, msg):
-    print("Message received on topic {0}: {1}"\
-        .format(msg.topic, msg.payload))
+    if msg.topic == 'hermes/hotword/default/detected':
+        pix.wakeup()
+        print("Wakeword detected!")
 
 client = mqtt.Client()
 client.on_connect = on_connect
