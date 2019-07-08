5ef6c2070c47aff232105954cda34afc55ad3381
Fabrice <thomasmartins83@gmail.com>
2019-07-08 12:39:53 +0200

    structuration du programme

--- action-lumiere.py
+++ action-lumiere.py
@@ -28,7 +28,7 @@ ALL_INTENTS = [INTENT_SET_LIGHT, INTENT_TURNON_LIGHT, INTENT_OPEN_BLINDS, INTENT
 
 pixels = pixel.Pixels()
 
-def init_wakeword(hermes,param):
+def begin_session(hermes,param):
     pixels.listen()
     print('WAKEWORD DETECTED')
 
@@ -36,60 +36,33 @@ def end_session(hermes,param):
     pixels.off();
     print('END OF THE SESSION')
 
-def intent_received(hermes, intent_message):
-    pixels.think();
-    print()
-    print(intent_message.intent.intent_name)
-    if intent_message.slots.window_devices:
-        print(intent_message.slots.window_devices[0].slot_value.value.value)
-    if intent_message.slots.state:
-        d_ouv = (intent_message.slots.state[0].slot_value.value.value).encode('utf-8')
+def ouvreStore(hermes, intent_message):
+    pixels.think()
+    if intent_message.slots.window_devices[0].slot_value.value.value == "stores":
+        if intent_message.slots.percentage:
+            d_ouv = str(intent_message.slots.percentage[0].slot_value.value.value)
+        else:
+            d_ouv = 0
+        requests.get("https://192.168.1.129:8443/UniversalListen?var1=VR&var2="+d_ouv+"&var3=BureauR8R9", verify=False)
+        hermes.publish_continue_session(intent_message.session_id, u"Autre choses ?", ALL_INTENTS)
+
+def fermeStore(hermes, intent_message):
+    pixels.think()
+    if intent_message.slots.window_devices[0].slot_value.value.value == "stores":
+        if intent_message.slots.closing_percent:
+            d_ouv = str(intent_message.slots.closing_percent[0].slot_value.value.value)
+        else:
+            d_ouv = 100
         print(d_ouv)
-    if intent_message.slots.window_state:
-        d_ouv = (intent_message.slots.window_state[0].slot_value.value.value).encode('utf-8')
-        print(d_ouv)
-    if intent_message.slots.percentage:
-        d_ouv = str(intent_message.slots.percentage[0].slot_value.value.value)
-        print(d_ouv)
-    if intent_message.slots.closing_percent:
-        d_ouv = str(intent_message.slots.closing_percent[0].slot_value.value.value)
-        print(d_ouv)
+        requests.get("https://192.168.1.129:8443/UniversalListen?var1=VR&var2=" + d_ouv + "&var3=BureauR8R9", verify=False)
+        hermes.publish_continue_session(intent_message.session_id, u"Autre choses ?", ALL_INTENTS)
 
-    if intent_message.intent.intent_name == "TomTom830:ModeScenario":
-        print(intent_message.slots.Mode[0].slot_value.value.value)
-        if intent_message.slots.Mode[0].slot_value.value.value == "Projection":
-            requests.get("https://192.168.1.129:8443/UniversalListen?var1=Scenario&var2=Projection&var3=BureauE11",verify=False)
-
-    if intent_message.intent.intent_name == "TomTom830:OpenCoverJeedom":
-        if intent_message.slots.window_devices[0].slot_value.value.value == "stores":
-            if intent_message.slots.percentage:
-                requests.get("https://192.168.1.129:8443/UniversalListen?var1=VR&var2="+d_ouv+"&var3=BureauR8R9", verify=False)
-
-            else:
-                requests.get("https://192.168.1.129:8443/UniversalListen?var1=VR&var2=0&var3=BureauR8R9", verify=False)
-
-    if intent_message.intent.intent_name == "TomTom830:CloseCover":
-        if intent_message.slots.window_devices[0].slot_value.value.value == "stores":
-            if intent_message.slots.closing_percent:
-                requests.get("https://192.168.1.129:8443/UniversalListen?var1=VR&var2=" + d_ouv + "&var3=BureauR8R9", verify=False)
-
-            else:
-                requests.get("https://192.168.1.129:8443/UniversalListen?var1=VR&var2=100&var3=BureauR8R9", verify=False)
-
-    if intent_message.intent.intent_name == "TomTom830:lightsSet":
-        hermes.publish_end_session(intent_message.session_id, u"J'allume la lumière")
-        requests.get("https://192.168.1.129:8443/UniversalListen?var1=Eclairage&var2=Allumer&var3=BureauE11", verify=False)
-        pixels.wakeup()
-    if intent_message.intent.intent_name == "bluevert:lightsTurnOff":
-        hermes.publish_end_session(intent_message.session_id, u"J'éteind la lumière")
-        requests.get("https://192.168.1.129:8443/UniversalListen?var1=Eclairage&var2=Eteindre&var3=BureauE11", verify=False)
-        pixels.off()
-    print()
-
-    if intent_message.intent.intent_name == "TomTom830:EndDialogue":
-        hermes.publish_end_session(intent_message.session_id, u"Au revoir")
+def finDialogue(hermes, intent_message):
+    pixels.think()
+    hermes.publish_end_session(intent_message.session_id, u"Au revoir")
 
 def mettreLumiere(hermes, intent_message):
+    pixels.think()
     if(intent_message.slots.intensity_percent):
         d_lum = str(intent_message.slots.intensity_percent[0].slot_value.value.value)
         print(d_lum)
@@ -98,8 +71,24 @@ def mettreLumiere(hermes, intent_message):
     requests.get("https://192.168.1.129:8443/UniversalListen?var1=Eclairage&var2="+d_lum+"&var3=BureauR8R9", verify=False)
     hermes.publish_continue_session(intent_message.session_id, u"Autre choses ?", ALL_INTENTS)
 
+def eteinsLumiere(hermes, intent_message):
+    pixels.think()
+    requests.get("https://192.168.1.129:8443/UniversalListen?var1=Eclairage&var2=0&var3=BureauR8R9",
+                 verify=False)
+    hermes.publish_continue_session(intent_message.session_id, u"Autre choses ?", ALL_INTENTS)
+
+def modeScenario(hermes, intent_message):
+    pixels.think()
+    print(intent_message.slots.Mode[0].slot_value.value.value)
+    mode = intent_message.slots.Mode[0].slot_value.value.value
+    requests.get("https://192.168.1.129:8443/UniversalListen?var1=Scenario&var2="+mode+"&var3=BureauE11",verify=False)
+    hermes.publish_continue_session(intent_message.session_id, u"Autre choses ?", ALL_INTENTS)
+
 with Hermes(MQTT_ADDR) as h:
     h.subscribe_intent("valf:lightsSetJeedom", mettreLumiere)\
-        .subscribe_intents(intent_received)\
-        .subscribe_session_started(init_wakeword)\
+        .subscribe_intent("TomTom830:EndDialogue", finDialogue)\
+        .subscribe_intent("TomTom830:CloseCover", fermeStore)\
+        .subscribe_intent("TomTom830:OpenCoverJeedom", ouvreStore)\
+        .subscribe_intent("valf:lightsTurnOffJeedom", eteinsLumiere).subscribe_intent("TomTom830:ModeScenario", modeScenario)\
+        .subscribe_session_started(begin_session)\
         .subscribe_session_ended(end_session).start()
